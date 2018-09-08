039bc91eb2ff37ada3c12c414652af5975c0c253
Amaan Marfatia <amaanmarfatia@gmail.com>
2018-09-08 16:36:17 -0400

    Complete BuildingBlockImpl

--- src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
+++ src/main/java/sf/codingcomp/blocks/solution/BuildingBlockImpl.java
@@ -3,37 +3,114 @@ package sf.codingcomp.blocks.solution;
 import java.util.Iterator;
 
 import sf.codingcomp.blocks.BuildingBlock;
+import sf.codingcomp.blocks.CircularReferenceException;
+
+import javax.swing.text.html.HTMLDocument;
 
 public class BuildingBlockImpl implements BuildingBlock {
 
+    BuildingBlock above, below;
+
     @Override
     public Iterator<BuildingBlock> iterator() {
-        // TODO Auto-generated method stub
-        return null;
+        BuildingBlock temp = this;
+        while (temp.findBlockUnder() != null) {
+            temp = temp.findBlockUnder();
+        }
+        return new BuildingBlockIterator(temp);
     }
 
     @Override
     public void stackOver(BuildingBlock b) {
-        // TODO Auto-generated method stub
-        
+        if (below != b) {
+            // Test for circularness
+            BuildingBlock curr = above;
+            while (curr != null) {
+                if (curr == b) {
+                    throw new CircularReferenceException();
+                }
+                curr = curr.findBlockOver();
+            }
+
+            BuildingBlock oldBelow = below;
+            below = null;
+            if (oldBelow != null) {
+                oldBelow.stackUnder(null);
+            }
+            below = b;
+            if (b != null) {
+                b.stackUnder(this);
+            }
+        }
     }
 
     @Override
     public void stackUnder(BuildingBlock b) {
-        // TODO Auto-generated method stub
-        
+        if (above != b) {
+            // Test for circularness
+            BuildingBlock curr = below;
+            while (curr != null) {
+                if (curr == b) {
+                    throw new CircularReferenceException();
+                }
+                curr = curr.findBlockUnder();
+            }
+
+            BuildingBlock oldAbove = above;
+            above = null;
+            if (oldAbove != null) {
+                oldAbove.stackOver(null);
+            }
+            above = b;
+            if (b != null) {
+                b.stackOver(this);
+            }
+        }
     }
 
     @Override
     public BuildingBlock findBlockUnder() {
-        // TODO Auto-generated method stub
-        return null;
+        return below;
     }
 
     @Override
     public BuildingBlock findBlockOver() {
-        // TODO Auto-generated method stub
-        return null;
+        return above;
     }
 
+    private class BuildingBlockIterator implements Iterator<BuildingBlock> {
+        BuildingBlock curr;
+        boolean removed;
+
+        public BuildingBlockIterator(BuildingBlock b) {
+            curr = b;
+            removed = true;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (curr != null);
+        }
+
+        @Override
+        public BuildingBlock next() {
+            removed = false;
+            BuildingBlock temp = curr;
+            curr = curr.findBlockOver();
+            return temp;
+        }
+
+        public void remove() {
+            BuildingBlock belowCurr = curr.findBlockUnder();
+            if (removed == true) {
+                throw new IllegalStateException();
+            }
+            removed = true;
+            if (belowCurr.findBlockUnder() == null) {
+                curr.stackOver(null);
+            } else {
+                curr.stackOver(belowCurr.findBlockUnder());
+            }
+        }
+    }
 }
